2e6bbd54ae989e46e1184244d4d872b7429ef198
Jerome Dassonville <dassonville.jerome@gmail.com>
2022-09-27 23:17:08 +0200

    Add tests

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ MONGO_URI = os.getenv("MONGO_URI", "localhost")
 MONGO_PORT = os.getenv("MONGO_PORT", 27017)
 MONGO_DB_NAME = "simplon"
 
+def format_messages_list(message):
+    html_text = "<ul>"
+    for message in message:
+        html_text += "<li>{}</li>".format(message["text"])
+    html_text += "</ul>"
+    return html_text
+
+
 @app.route("/")
 def hello_world():
     mongodb_client = MongoClient(MONGO_URI, MONGO_PORT)
     simplon_db = mongodb_client[MONGO_DB_NAME]
-
-    html_text = "<ul>"
-    for message in simplon_db.messages.find():
-        html_text += "<li>{}</li>".format(message["text"])
-
-    html_text += "</ul>"
-    return html_text
+    return format_messages_list(simplon_db.messages.find())
---
+++ app_test.py
@@ -0,0 +1,6 @@
+from app import format_messages_list
+
+def test_format_message():
+    messages = [{"text": "hello world"}]
+    result = format_messages_list(messages)
+    assert result == "<ul><li>hello world</li></ul>"
